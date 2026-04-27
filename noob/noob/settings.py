928145df12e05a1a1876261cd3a82ee3dd189aab
Lumecather <vs1914sochi@gmail.com>
2026-04-27 18:02:41 +0300

    deploy fix

--- noob/noob/settings.py
+++ noob/noob/settings.py
@@ -77,11 +77,13 @@ WSGI_APPLICATION = 'noob.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
 }
 
 # Password validation
@@ -119,7 +121,9 @@ USE_TZ = True
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 INTERNAL_IPS = [
     '127.0.0.1',
